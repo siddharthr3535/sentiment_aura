78a9118329092b8d5d98134499ac2b3468d290d5
Sid <siddharthr3535@gmail.com>
2025-11-19 17:31:29 -0500

    changing from vercel to netlify

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173","https://*.vercel.app"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173","https://*.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
